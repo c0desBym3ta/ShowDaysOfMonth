3804fb737c5d5215d2a36dc69df1e15ee7dd6508
cllhunifi <daniel.cullhaj@stud.unifi.it>
2017-07-23 15:39:42 +0100

    Implementing show calendar function and main test.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,49 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 int isLeap(int year);
 int totalDays(int year, int month);
 int start(int year, int month);
+int showCalendar(int year, int month);
 
 int main() {
+    int year, month;
+    cout << "\nEnter year: "; cin >> year;
+    cout << "Enter month: "; cin >> month;
+    showCalendar(year,month);
+    cout << endl;
 
-
-
+    system("pause");
     return 0;
 }
 
 int start(int year, int month){
-    
+    int table[] = {0,3,2,5,0,3,5,1,4,6,2,4};
+    int y =year-(month<3);
+    return (year + year/4 -year/100 + year/400 + table[month-1] + 1)%7;
+}
+
+int showCalendar(int year, int month){
+    string monthName [] = {"January","February","March","April","May","June","July","August","September","October","November","December"};
+    int day, days, dow;
+
+    days=totalDays(year,month);
+    dow=start(year,month);
+    cout << endl;
+    cout << "\t" << monthName[month-1] << " " << year << endl;
+    cout << " SUN MON TUE WED THU FRI SUT" << endl;
+    cout << " --- --- --- --- --- --- ---" << endl;
+    for(auto i = 0; i<dow; i++ ){
+        cout << "    ";
+    }
+    for(day = 1; day < days; day++){
+        cout << setw(4) << right << day;
+        if(++dow>6){ dow = 0; cout << endl;}
+    }
+    cout << endl;
+
 }
 
 int isLeap(int year){
